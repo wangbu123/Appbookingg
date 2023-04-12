327afffc9e1cccb3921a9ebbb038de2c2a9c8c53
wangbu123 <haoquang969@gmail.com>
2023-04-13 00:00:27 +0700

    up lan dau

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:app_booking/home/main_food_page.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.lightBlue,
+      ),
+      home: MainFoodPage(),
+    );
+  }
+}
